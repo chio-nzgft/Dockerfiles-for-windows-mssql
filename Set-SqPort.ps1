5a7c99d4a6084d3f938618cb56460bb3aa6fa66a
echochio <chio-nzgft@users.noreply.github.com>
2017-04-10 09:00:59 +0800

    Update Set-SqPort.ps1

--- Set-SqPort.ps1
+++ Set-SqPort.ps1
@@ -12,6 +12,6 @@ $servicename = "mssql$" + $sqlinstance
 
 stop-service $servicename
 
-set-itemproperty -path "HKLM:\software\microsoft\microsoft sql server\mssql12.$sqlinstance\mssqlserver\supersocketnetlib\tcp\ipall" -name tcpdynamicports -value ""
-set-itemproperty -path "HKLM:\software\microsoft\microsoft sql server\mssql12.$sqlinstance\mssqlserver\supersocketnetlib\tcp\ipall" -name tcpport -value 1433
+set-itemproperty -path "HKLM:\software\microsoft\microsoft sql server\mssql1.$sqlinstance\mssqlserver\supersocketnetlib\tcp\ipall" -name tcpdynamicports -value ""
+set-itemproperty -path "HKLM:\software\microsoft\microsoft sql server\mssql1.$sqlinstance\mssqlserver\supersocketnetlib\tcp\ipall" -name tcpport -value 1433
 start-service $servicename
